88c190be88aa24e57d6000148938c39af3d99103
Ganesh Jangal <ganesh.jangal@outlook.com>
2018-04-21 17:38:09 -0400

    Added teable definitions

---
+++ PortfolioTracker.Db/Tables/Lot.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Lot]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Ric] VARCHAR(24) NULL, 
+    [Price] FLOAT NULL, 
+    [Qty] FLOAT NULL, 
+    [Date] DATETIME NULL, 
+    [Commission] FLOAT NULL
+)
---
+++ PortfolioTracker.Db/Tables/MarketData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MarketData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Ric] VARCHAR(24) NULL, 
+    [MktPrice] FLOAT NULL, 
+    [LastPx] FLOAT NULL, 
+    [Date] DATETIME NULL
+)
