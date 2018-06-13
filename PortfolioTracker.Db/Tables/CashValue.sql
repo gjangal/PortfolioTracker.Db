528e8aed6228c9c62b9447ca1aa7aa1cfa203ac8
Ganesh Jangal <ganesh.jangal@outlook.com>
2018-06-12 22:25:00 -0400

    made id column as identity

--- PortfolioTracker.Db/Tables/CashValue.sql
+++ PortfolioTracker.Db/Tables/CashValue.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].CashValue
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
     [PortfolioId] INT NULL, 
     [Cash] FLOAT NULL, 
     [Date] DATETIME NULL
